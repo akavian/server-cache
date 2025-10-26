99674f768e63719e05d631f8653e44b8554072b3
Ali <akavian@outlook.com>
2025-10-26 18:27:20 +0100

    initialize the project.

---
+++ src/main/kotlin/com/ali/server/cache/ServerCacheApplication.kt
@@ -0,0 +1,11 @@
+package com.ali.server.cache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerCacheApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServerCacheApplication>(*args)
+}
---
+++ src/test/kotlin/com/ali/server/cache/ServerCacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ali.server.cache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerCacheApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
