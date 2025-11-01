ec745a270e5677c0b8d9d9f0ccc958563470a221
Ali <akavian@outlook.com>
2025-11-01 18:26:41 +0100

    Adding direct service to be used when caching is not desired.

---
+++ src/main/kotlin/com/ali/server/cache/service/impl/DirectResourceService.kt
@@ -0,0 +1,34 @@
+package com.ali.server.cache.service.impl
+
+import com.ali.server.cache.model.Resource
+import com.ali.server.cache.service.ResourceService
+import org.springframework.http.ETag
+import org.springframework.stereotype.Service
+
+@Service(ResourceService.DIRECT_SERVICE)
+class DirectResourceService: ResourceService {
+
+    override fun getResource(
+        nameSpace: String,
+        id: String,
+        eTag: ETag?
+    ): Resource {
+        TODO("Not yet implemented")
+    }
+
+    override fun getManyResourcesInNameSpace(ids: List<String>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun putResource(
+        nameSpace: String,
+        id: String,
+        resource: Resource
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteResource(nameSpace: String, id: String) {
+        TODO("Not yet implemented")
+    }
+}
