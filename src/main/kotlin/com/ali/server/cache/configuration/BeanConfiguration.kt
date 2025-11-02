8673c8e5e219076104fc681ca4e196820d17b33e
Ali <akavian@outlook.com>
2025-11-02 02:48:50 +0100

    Creating a custom strategy map

---
+++ src/main/kotlin/com/ali/server/cache/configuration/BeanConfiguration.kt
@@ -0,0 +1,23 @@
+package com.ali.server.cache.configuration
+
+import com.ali.server.cache.enums.ResourceStrategy
+import com.ali.server.cache.enums.StrategyMap
+import com.ali.server.cache.exception.StrategyNotFoundException
+import com.ali.server.cache.service.ResourceService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BeanConfiguration {
+
+    @Bean
+    fun strategyMap(beans: Map<String, ResourceService>): StrategyMap = mapOf(
+        ResourceStrategy.CACHED to (beans[ResourceService.DIRECT_SERVICE] ?: throw StrategyNotFoundException(
+            ResourceStrategy.CACHED
+        )),
+        ResourceStrategy.DIRECT to (beans[ResourceService.CACHED_SERVICE] ?: throw StrategyNotFoundException(
+            ResourceStrategy.DIRECT
+        ))
+    )
+
+}
