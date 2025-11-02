fa914960dc28a101a6413714f9f4cd967ff8334d
Ali <akavian@outlook.com>
2025-11-02 01:16:44 +0100

    Define an exception for strategies delegation and selection

---
+++ src/main/kotlin/com/ali/server/cache/exception/StrategyNotFoundException.kt
@@ -0,0 +1,7 @@
+package com.ali.server.cache.exception
+
+import com.ali.server.cache.enums.ResourceStrategy
+
+class StrategyNotFoundException(strategy: ResourceStrategy) :
+    IllegalStateException("Strategy $strategy is not defined") {
+}
