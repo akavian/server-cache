e0cae080a0de904d7c13e9e875712305d6146ec5
Ali <akavian@outlook.com>
2025-11-03 23:47:41 +0100

    adding jsr310

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+
 }
 
 kotlin {
