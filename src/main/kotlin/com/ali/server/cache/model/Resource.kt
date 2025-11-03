b22bbf6e92f4711d2de8a4bec3211ac9533d483c
Ali <akavian@outlook.com>
2025-11-04 00:48:05 +0100

    Refactoring Resource.kt

--- src/main/kotlin/com/ali/server/cache/model/Resource.kt
+++ src/main/kotlin/com/ali/server/cache/model/Resource.kt
@@ -3,7 +3,7 @@ package com.ali.server.cache.model
 import java.time.Instant
 
 data class Resource(
-    val id: Long,
+    val id: String,
     val nameSpace: String,
     val content: Map<String, Any?>,
     val updatedAt: Instant,
