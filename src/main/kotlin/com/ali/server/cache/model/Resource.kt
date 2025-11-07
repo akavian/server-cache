581841bbfef4317a1a2a473df3184c68188f5792
Ali <akavian@outlook.com>
2025-11-07 23:31:33 +0100

    Modifying and adding extra annotations and key creation for Resource.

--- src/main/kotlin/com/ali/server/cache/model/Resource.kt
+++ src/main/kotlin/com/ali/server/cache/model/Resource.kt
@@ -1,11 +1,26 @@
 package com.ali.server.cache.model
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
+@Document("resources")
 data class Resource(
+
     val id: String,
+    @Indexed(name = "idx_namespace")
     val nameSpace: String,
     val content: Map<String, Any?>,
     val updatedAt: Instant,
     val version: Long
-)
+) {
+    @get:Id
+    val key: String
+        get() = "$nameSpace;$id"
+
+    companion object {
+        fun getKey(ns: String, id: String) = "$ns:$id"
+    }
+}
+
