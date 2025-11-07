7cead4f4b58ba2a0cb38b782c31962265de879e7
Ali <akavian@outlook.com>
2025-11-07 23:29:29 +0100

    Cleaning up extra brackets.

--- src/main/kotlin/com/ali/server/cache/exception/StrategyNotFoundException.kt
+++ src/main/kotlin/com/ali/server/cache/exception/StrategyNotFoundException.kt
@@ -3,5 +3,4 @@ package com.ali.server.cache.exception
 import com.ali.server.cache.enums.ResourceStrategy
 
 class StrategyNotFoundException(strategy: ResourceStrategy) :
-    IllegalStateException("Strategy $strategy is not defined") {
-}
+    IllegalStateException("Strategy $strategy is not defined")
