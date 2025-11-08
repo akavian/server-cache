dca172bed390ba1dc062415dea2a7891508f5b71
Ali <akavian@outlook.com>
2025-11-08 01:56:01 +0100

    Clean up.

--- src/main/kotlin/com/ali/server/cache/configuration/StrategyMapBeanConfiguration.kt
+++ src/main/kotlin/com/ali/server/cache/configuration/StrategyMapBeanConfiguration.kt
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class BeanConfiguration {
+class StrategyMapBeanConfiguration {
 
     @Bean
     fun strategyMap(beans: Map<String, ResourceService>): StrategyMap = mapOf(
@@ -19,5 +19,4 @@ class BeanConfiguration {
             ResourceStrategy.DIRECT
         ))
     )
-
 }
