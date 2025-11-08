7984c9be38b96b9bfeed077503aa674ea27c8652
Ali <akavian@outlook.com>
2025-11-08 02:39:35 +0100

    Adding caffeine cache configuration.

---
+++ src/main/kotlin/com/ali/server/cache/configuration/ResourceCacheBeanConfiguration.kt
@@ -0,0 +1,25 @@
+package com.ali.server.cache.configuration
+
+import com.ali.server.cache.configuration.properties.ResourceCacheProperties
+import com.ali.server.cache.exception.ResourceNotFoundException
+import com.ali.server.cache.model.Resource
+import com.ali.server.cache.repository.ResourceRepository
+import com.github.benmanes.caffeine.cache.Caffeine
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.util.concurrent.TimeUnit
+
+@Configuration
+@ConfigurationPropertiesScan
+class ResourceCacheBeanConfiguration(val resourceCacheProperties: ResourceCacheProperties) {
+
+    @Bean
+    fun resourceCache(resourceRepository: ResourceRepository) =
+        Caffeine.newBuilder()
+            .expireAfterWrite(resourceCacheProperties.expireAfterWrite, TimeUnit.SECONDS)
+            .initialCapacity(resourceCacheProperties.initialCapacity)
+            .build<String, Resource> {
+                resourceRepository.findById(it).orElseThrow { ResourceNotFoundException(it) }
+            }
+}
