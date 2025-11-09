e3bbec112029005536644aab7a442e008c437eef
Ali <akavian@outlook.com>
2025-11-09 18:14:57 +0100

    Enable auditing for mongo client

--- src/main/kotlin/com/ali/server/cache/configuration/MongoBeanConfiguration.kt
+++ src/main/kotlin/com/ali/server/cache/configuration/MongoBeanConfiguration.kt
@@ -8,8 +8,10 @@ import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableMongoAuditing
 
 @Configuration
+@EnableMongoAuditing
 class MongoBeanConfiguration(val mongoProperties: MongoProperties) {
 
     @Bean
