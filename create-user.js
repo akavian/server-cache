1a3098743308d2aa7ff362a992f13e566c36d49d
Ali <akavian@outlook.com>
2025-11-09 15:44:42 +0100

    Adjusting/adding the environment variables.

--- create-user.js
+++ create-user.js
@@ -1,8 +1,9 @@
-const dbName = process.env.MONGODB_DB || "server-cache-mongo";
-const appUser = process.env.MONGODB_APP_USER || "appuser";
+const dbName = process.env.MONGODB_DATABASE || "server-cache-mongo";
+const authDbname = process.env.MONGO_DB_AUTH || dbName;
+const appUser = process.env.MONGODB_APP_USER || "appUser";
 const appPass = process.env.MONGODB_APP_PASS || "appPass";
 
-const db = new Mongo().getDB(dbName);
+const db = new Mongo().getDB(authDbname);
 
 db.createUser({
   user: appUser,
