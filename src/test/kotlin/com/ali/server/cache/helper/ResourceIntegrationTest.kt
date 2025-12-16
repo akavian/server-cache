91a357c8eaf88183f5bc9e3ecb286ee5c4a84365
Ali <akavian@outlook.com>
2025-12-16 23:36:38 +0100

    Creating the ResourceIntegrationTest.kt

---
+++ src/test/kotlin/com/ali/server/cache/helper/ResourceIntegrationTest.kt
@@ -0,0 +1,13 @@
+package com.ali.server.cache.helper
+
+import com.ali.server.cache.integration.AbstractIntegrationTest
+import kotlin.test.Test
+
+class ResourceIntegrationTest: AbstractIntegrationTest() {
+
+    @Test
+    fun `Test resource creation`() {
+
+    }
+
+}
