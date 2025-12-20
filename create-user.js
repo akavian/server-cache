a835eb3b080d08ce2c71b9dad1f7e9b6be032ac9
Ali <akavian@outlook.com>
2025-12-20 22:43:37 +0100

    Correcting env variable names.

--- create-user.js
+++ create-user.js
@@ -1,4 +1,4 @@
-const dbName = process.env.MONGODB_DATABASE || "server-cache-mongo";
+const dbName = process.env.MONGO_DATABASE || "server-cache-mongo";
 const authDbname = process.env.MONGO_DB_AUTH || dbName;
 const appUser = process.env.MONGODB_APP_USER || "appUser";
 const appPass = process.env.MONGODB_APP_PASS || "appPass";
