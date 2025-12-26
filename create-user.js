8fd87c6fd73b18505ab0720b06f6962c9c956f15
Ali <akavian@outlook.com>
2025-12-26 01:59:45 +0100

    Fixing the script as the env variable was not passed to mongodb in compose.

--- create-user.js
+++ create-user.js
@@ -1,7 +1,7 @@
 const dbName = process.env.MONGO_DATABASE || "server-cache-mongo";
-const authDbname = process.env.MONGO_DB_AUTH || dbName;
-const appUser = process.env.MONGODB_APP_USER || "appUser";
-const appPass = process.env.MONGODB_APP_PASS || "appPass";
+const authDbname = process.env.MONGO_AUTH_DB || "admin";
+const appUser = process.env.MONGO_USERNAME || "appUser";
+const appPass = process.env.MONGO_PASSWORD || "appPass";
 
 const db = new Mongo().getDB(authDbname);
 
