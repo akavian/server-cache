27ad811d6c7abee07b23c02066a968d09b3f6b13
Ali <akavian@outlook.com>
2025-12-30 02:53:54 +0100

    Adding default value for the const parameter

--- src/main/kotlin/com/ali/server/caching/exception/ResourceNotFoundException.kt
+++ src/main/kotlin/com/ali/server/caching/exception/ResourceNotFoundException.kt
@@ -2,7 +2,7 @@ package com.ali.server.caching.exception
 
 import java.lang.RuntimeException
 
-class ResourceNotFoundException(key: String?) :
+class ResourceNotFoundException(key: String? = null) :
     RuntimeException(
         key?.let { "Resource $key not found." } ?: "No resource found."
     )
